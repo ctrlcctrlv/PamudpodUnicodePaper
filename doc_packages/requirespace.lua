72a7f4a90830701bdcb928bd9b9fe44d14d4b840
Fredrick Brennan <copypaste@kittens.ph>
2020-08-12 17:57:10 -0400

    Initial commit

---
+++ doc_packages/requirespace.lua
@@ -0,0 +1,11 @@
+SILE.registerCommand("requireSpace", function (options, content)
+  local required = SILE.length(options.height or 0)
+  SILE.typesetter:leaveHmode()
+  SILE.call("hbox", {}, content) -- push content we want to fit
+  local heightOfPageSoFar = SILE.pagebuilder:collateVboxes(SILE.typesetter.state.outputQueue).height
+  local heightOfFrame = SILE.typesetter.frame:height()
+  table.remove(SILE.typesetter.state.nodes) -- steal it back
+  if heightOfFrame - heightOfPageSoFar < required then
+    SILE.call("supereject")
+  end
+end)
